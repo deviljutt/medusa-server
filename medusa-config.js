36152580276a702b5c345347142f5450726b08a3
Microtica <info@microtica.com>
2023-05-26 09:14:53 +0000

    Initialized from 'Medusa Server' template

---
+++ medusa-config.js
@@ -0,0 +1,110 @@
+const dotenv = require("dotenv");
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) { }
+
+// CORS when consuming Medusa from admin
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+
+const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: `medusa-file-s3`,
+    options: {
+      s3_url: process.env.S3_URL,
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION
+    },
+  },
+  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: false,
+    },
+  },
+];
+
+const modules = {
+  /*eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },*/
+}
+
+// Extend project configuration with Microtica specific configuration logic
+const projectConfigExt = process.env.PRODUCTION === "true" ?
+  // Production configuration
+  {
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres"
+  } :
+  // Development configuration
+  {
+    database_database: "./.tmp/medusa-db.sql",
+    database_type: "sqlite"
+  }
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  database_database: "./medusa-db.sql",
+  database_type: DATABASE_TYPE,
+  store_cors: STORE_CORS,
+  admin_cors: ADMIN_CORS,
+  // Uncomment the following lines to enable REDIS
+  // redis_url = REDIS_URL
+
+  ...projectConfigExt
+}
+
+if (DATABASE_URL && DATABASE_TYPE === "postgres") {
+  projectConfig.database_url = DATABASE_URL;
+  delete projectConfig["database_database"];
+}
+
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
+  plugins,
+  modules,
+};
